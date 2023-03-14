cbabb81f0883c5a9b07b99a421a9bd7e20a2f477
Gabriel Claudino <gaclaudino.13@gmail.com>
2023-03-14 12:28:36 -0300

    Test drive
    Programa com: Objetos Visuais .NET + Objetos de banco de dados .NET + Armazenamento de Dados.
    Pendente: Debugar e melhorar aparecia.
    Próximo passo: Criar o pacote de distribuição.

---
+++ Contatos/Contatos/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List 1.0. \nWritten by: Gabriel Claudino", "About");
+        }
+
+        private void pessoaBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pessoaBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'contactDBDataSet.Pessoa' table. You can move, or remove it, as needed.
+            this.pessoaTableAdapter.Fill(this.contactDBDataSet.Pessoa);
+
+        }
+    }
+}
---
+++ Contatos/Contatos/dbo.Pessoa.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Pessoa]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NCHAR(50) NULL, 
+    [Company] NCHAR(50) NULL, 
+    [Telephone] NCHAR(50) NULL, 
+    [Email] NCHAR(50) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL
+
+)
