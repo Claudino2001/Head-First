494697a232cd347967b5d99b52e70fb593ce37b6
Gabriel Claudino <gaclaudino.13@gmail.com>
2023-03-18 23:12:39 -0300

    3 Talker Tester

---
+++ 3 Talker Tester/3 Talker Tester/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_Talker_Tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int len = Talker.BlahBlahBlah(textBox1.Text, (int)numericUpDown1.Value);
+            MessageBox.Show("The message lenght is " + len);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Written by Gabriel Claudino\nDate: 18/03/2023");
+        }
+    }
+}
