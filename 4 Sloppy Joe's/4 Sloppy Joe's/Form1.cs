9e33360c4e44b23ddc7dc933282c9a054c948e08
Gabriel Claudino <gaclaudino.13@gmail.com>
2023-03-31 13:57:43 -0300

    4 Sloppy Joe's

---
+++ 4 Sloppy Joe's/4 Sloppy Joe's/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _4_Sloppy_Joe_s
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gabriel();
+        }
+
+        public void gabriel()
+        {
+            MenuMaker menu = new MenuMaker() { Randomizer = new Random() };
+            label1.Text = menu.GetMenuItem();
+            label2.Text = menu.GetMenuItem();
+            label3.Text = menu.GetMenuItem();
+            label4.Text = menu.GetMenuItem();
+            label5.Text = menu.GetMenuItem();
+            label6.Text = menu.GetMenuItem();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gabriel();
+        }
+    }
+}
---
+++ 4 Sloppy Joe's/4 Sloppy Joe's/MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Sloppy_Joe_s
+{
+    internal class MenuMaker
+    {
+        public Random Randomizer;
+        string[] Meats = {"Roast beef", "Salami", "Turkey", "Ham", "Pastrami"};
+        string[] Condiments = {"yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        string[] Breads = { "rye", "white", "wheat", "pumpernickel", "italian bread", "a roll"};
+        
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string ranromCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string ranromBreads = Condiments[Randomizer.Next(Breads.Length)];
+
+            return randomMeat + " with " + ranromCondiment + " on "+ ranromBreads + ".";
+        }
+
+    }
+}
